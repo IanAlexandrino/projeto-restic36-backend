de7df30b0fd70cee7c36c932e197a1efc0ad03c2
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-11-24 03:33:03 -0300

    feat: adding functionals views to the institution entity and small changes in url archives

--- apps/institutions/urls.py
+++ apps/institutions/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from apps.institutions.views import institutions, institutions_types
+
+from . import views
 
 urlpatterns = [
-    path('institutions/', institutions),
-    path('institutions-types/', institutions_types),
+    path('', views.get_all_and_post_institutions, name='get_all_and_post_institutions'),
+    path('<int:id>/', views.get_put_and_delete_institution_by_id, name='get_institution_by_id'),
+    path('types/', views.get_all_and_post_institution_types, name='get_all_and_post_intitution_types'),
+    path('types/<int:id>/', views.get_put_and_delete_institution_type_by_id, name='get_put_and_delete_institution_type_by_id'),
 ]
--- apps/institutions/views.py
+++ apps/institutions/views.py
@@ -1,9 +1,119 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-def institutions(request):
-    return HttpResponse('institutions')
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+from .models import Institution, InstitutionType
+from .serializers import InstitutionSerializer, InstitutionTypeSerializer
 
-def institutions_types(request):
-    return HttpResponse('institutions types')
+import json
+
+@api_view(['GET', 'POST'])
+def get_all_and_post_institution_types(request):
+
+    if request.method == 'GET':
+        intitution_types = InstitutionType.objects.all()
+
+        serializer = InstitutionTypeSerializer(intitution_types, many=True)
+
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        new_institution_type = request.data
+
+        serializer = InstitutionTypeSerializer(data=new_institution_type)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_put_and_delete_institution_type_by_id(request, id):
+
+    try:
+        institution_type = InstitutionType.objects.get(pk=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = InstitutionTypeSerializer(institution_type)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = InstitutionTypeSerializer(institution_type, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+
+        try:
+            institution_type.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST'])
+def get_all_and_post_institutions(request):
+
+    if request.method == 'GET':
+        intitutions = Institution.objects.all()
+
+        serializer = InstitutionSerializer(intitutions, many=True)
+
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        new_institution = request.data
+
+        serializer = InstitutionSerializer(data=new_institution)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_put_and_delete_institution_by_id(request, id):
+
+    try:
+        institution = Institution.objects.get(pk=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = InstitutionSerializer(institution)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = InstitutionSerializer(institution, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+
+        try:
+            institution.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
