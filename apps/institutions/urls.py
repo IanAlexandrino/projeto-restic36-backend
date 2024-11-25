2119d7cf54e00b7a39152eb5fbf6e74fb3217f84
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-11-25 09:27:08 -0300

    fix: changing the url name

--- apps/institutions/urls.py
+++ apps/institutions/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.get_all_and_post_institutions, name='get_all_and_post_institutions'),
-    path('<int:id>/', views.get_put_and_delete_institution_by_id, name='get_institution_by_id'),
+    path('<int:id>/', views.get_put_and_delete_institution_by_id, name='get_put_and_delete_institution_by_id'),
     path('types/', views.get_all_and_post_institution_types, name='get_all_and_post_intitution_types'),
     path('types/<int:id>/', views.get_put_and_delete_institution_type_by_id, name='get_put_and_delete_institution_type_by_id'),
 ]
